2b9d8e70bd3643cab56647df62926715a4a11514
felipecastillo.snk <felipecastillo.snk@gmail.com>
2024-06-18 17:23:55 +0000

    finish setup project

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color colorRed = Color(0xFFDA4040);
+const Color colorBlue = Color(0xFF5F52EE);
+const Color colorBlack = Color(0xFF3A3A3A);
+const Color colorGrey = Color(0xFF717171);
+const Color colorBackground = Color(0xFFEEEFF5);
