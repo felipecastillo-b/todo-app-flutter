35259310241a5b719672db02d1d30127e58fe3d2
felipecastillo.snk <felipecastillo.snk@gmail.com>
2024-06-18 18:43:37 +0000

    add appbar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -6,10 +7,35 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Todo App')),
+      backgroundColor: colorBackground,
+      appBar: _buildAppBar(),
       body: Container(
         child: Text('This is home screen'),
       ),
       );
+  }
+
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: colorBackground,
+      elevation: 0,
+      title: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+        Icon(
+          Icons.menu,
+          color: colorBlack,
+          size: 30,
+          ),
+          Container(
+            height: 40,
+            width: 40,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.asset('assets/images/avatar.jpeg'),
+              ),
+          )
+      ]),
+    );
   }
 }
